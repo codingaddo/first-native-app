22ba5b24f9941a5a9ff087e2b1020087f19dc333
Addo Michael <michaeladdo1997@gmail.com>
2024-06-18 17:54:42 +0000

    .

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,46 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "../../constants/Colors";
+
+const TabLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: Colors.primaryColor,
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          tabBarLabel: "Explore",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="people-circle" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabLayout;
